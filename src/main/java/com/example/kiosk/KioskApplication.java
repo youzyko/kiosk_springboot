954d72b21e80dd96c06dcb7e55ec00dc94c80128
youzyko <gokongjin@naver.com>
2023-08-24 14:30:59 +0900

    please

--- src/main/java/com/example/kiosk/KioskApplication.java
+++ src/main/java/com/example/kiosk/KioskApplication.java
@@ -2,9 +2,10 @@ package com.example.kiosk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class KioskApplication {
+public class KioskApplication extends SpringBootServletInitializer {
 
 
 	public static void main(String[] args) {

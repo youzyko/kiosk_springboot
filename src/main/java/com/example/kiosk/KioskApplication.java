8fef08e72f487fcb69cbfb229e6dfa4c3aa9b574
youzyko <gokongjin@naver.com>
2023-08-31 10:39:48 +0900

    20230831_1

--- src/main/java/com/example/kiosk/KioskApplication.java
+++ src/main/java/com/example/kiosk/KioskApplication.java
@@ -6,11 +6,11 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class KioskApplication extends SpringBootServletInitializer  {
-	@Override
+public class KioskApplication /*extends SpringBootServletInitializer*/  {
+	/*@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(KioskApplication.class);
-	}
+	}*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(KioskApplication.class, args);
